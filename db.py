c79027bb20fcd3138059ab9451705aa0475bea43
macadamia-code <macadamia.code@gmail.com>
2025-06-01 18:25:21 +0900

    MYSQL対応

--- db.py
+++ db.py
@@ -1,8 +1,23 @@
 import os
-import psycopg2
 from dotenv import load_dotenv
+import mysql.connector
+import psycopg2
 
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(os.getenv("DATABASE_URL"))
+    db_type = os.getenv("DB_TYPE")
+
+    if db_type == "postgresql":
+        # PostgreSQL用接続
+        return psycopg2.connect(os.getenv("PGSQL_DATABASE_URL"))
+    elif db_type == "mysql":
+        # MySQL用接続
+        return mysql.connector.connect(
+            host=os.getenv("MYSQL_HOST"),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DATABASE"),
+        )
+    else:
+        raise ValueError(f"Unsupported DB_TYPE: {db_type}")
